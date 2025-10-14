f55484cfe9b2dca6c59740561f8945ff76298db9
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-14 19:10:47 +0200

    ejercicio seleccionar favorito finalizado

--- src/components/PadreDeportes.js
+++ src/components/PadreDeportes.js
@@ -3,17 +3,27 @@ import HijoDeporte from "./HijoDeporte";
 
 class PadreDeportes extends Component {
 
+    state = {
+        favorito: ""
+    }
+
+    marcarFavorito = (nombreDeporte) => {
+        this.setState({
+            favorito: nombreDeporte
+        })
+    }
+
     deportes = ["Baloncesto", "futbol", "tenis", "hockey"];
 
     render () {
         return(
             <div>
                 <h1>Padre deportes</h1>
-                <h3 style={{backgroundColor: "yellow"}}>Su deporte favorito es:</h3>
+                <h3 style={{backgroundColor: "yellow"}}>Su deporte favorito es: {this.state.favorito}</h3>
 
                 {
                     this.deportes.map((nombre, index) => {
-                        return(<HijoDeporte key={index} nombre={nombre} />)
+                        return(<HijoDeporte key={index} nombre={nombre} marcarFavorito={this.marcarFavorito}/>)
                     })
                 }
             </div>
